867f1515cf76a1665d6c086be45700512ae53119
Yamemik <kuancarlos@mail.ru>
2024-03-30 15:07:38 +0500

    + новая почта c почтой

--- services/auth_service.py
+++ services/auth_service.py
@@ -112,7 +112,7 @@ def send_email(text, email):
     msg.attach(MIMEText(text, 'plain'))
 
     smtpObj = smtplib.SMTP('smtp.gmail.com')
-    smtpObj.starttls()
+    # smtpObj.starttls()
     smtpObj.login(msg['From'], 'wknl lnwz yuzg wmul')
     smtpObj.sendmail(msg['From'], msg['To'], msg.as_string())
     smtpObj.quit()
